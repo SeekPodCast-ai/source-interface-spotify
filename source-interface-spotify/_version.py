b3acc66ebfdef6b7474b0d8d57647c6692e1b5d9
Shruti <patangeshruti1@gmail.com>
2024-10-05 15:55:21 +0530

    fix: trying to fix auto version bump

---
+++ source-interface-spotify/_version.py
@@ -0,0 +1,9 @@
+from pathlib import Path
+import toml
+
+def get_version():
+    # Load the version from pyproject.toml
+    pyproject = toml.load(Path(__file__).parent.parent / 'pyproject.toml')
+    return pyproject['tool']['poetry']['version']
+
+__version__ = get_version()
