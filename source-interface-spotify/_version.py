c1d0868b97aeb8a15189e3f6cfe00e27551eb595
Shruti <patangeshruti1@gmail.com>
2024-10-05 15:57:52 +0530

    fix: fixing linting issues

--- source-interface-spotify/_version.py
+++ source-interface-spotify/_version.py
@@ -1,9 +1,11 @@
 from pathlib import Path
 import toml
 
+
 def get_version():
     # Load the version from pyproject.toml
     pyproject = toml.load(Path(__file__).parent.parent / 'pyproject.toml')
     return pyproject['tool']['poetry']['version']
+
 
 __version__ = get_version()
